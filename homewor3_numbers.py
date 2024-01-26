8826d8d66a4e8b83bd2c7e792eb913dbb1b3ed38
NataliiaSpas2002 <nataliyaspasenko@gmail.com>
2024-01-26 15:39:36 +0200

    homework3_numbers

---
+++ homewor3_numbers.py
@@ -0,0 +1,51 @@
+# Homework_3
+# Task 1
+
+sales_revenue_forecast = 1000000
+sales_ebitda_forecast = sales_revenue_forecast * 0.23
+print(f'Sales EBITDA forecast: {sales_ebitda_forecast}')
+
+# Task 2
+
+land_area = 10000
+land_area_in_acres = land_area / 4047
+formatted_land_area_in_acres = "{:.2f}".format(land_area_in_acres)
+print(f'Land area in acres: {formatted_land_area_in_acres}')
+
+# Task 3
+
+purchase_amount = 110
+federal_tax = purchase_amount * 0.05
+regional_tax = purchase_amount * 0.025
+total_tax = federal_tax + regional_tax
+total_sle_amount = purchase_amount + total_tax
+print(f'Purchase amount: {purchase_amount}, Federal tax: {federal_tax},'
+      f'\nRegional tax: {regional_tax}, Total tax: {total_tax}, Total sales amount: {total_sle_amount}')
+
+# Task 4
+# Сделайте так, чтобы число секунд отображалось в виде <дни:часы:минуты:секунды>.
+
+total_seconds = 200000
+days = total_seconds // 86400
+hours = total_seconds % 86000 // 3600
+minutes = total_seconds % 86400 % 3600 // 60
+seconds = total_seconds % 86400 % 3600 % 60
+
+print(f'There is {days} days {hours} hours {minutes} minutes and {seconds} in {total_seconds}')
+
+# Task 5
+# При заданном целом числе n посчитайте n + nn + nnn.
+my_number = 10
+
+print(int(my_number) + int(my_number) * 2 + int(my_number) * 3)
+
+# Task 6
+speed_in_km_h = 1000
+speed_in_m_sec = 1000
+converted_speed = speed_in_km_h * 1000 / 3600
+if converted_speed < speed_in_m_sec:
+      print(f'{speed_in_km_h} kilometers per second is less then {speed_in_m_sec} meters per second')
+
+
+
+
